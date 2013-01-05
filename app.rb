25ef159e16fecc885273bd31ccf1dbdad42aa445
Gabriele Cirulli <gabrycir@gmail.com>
2013-01-06 00:36:49 +0100

    playlist loading route

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'open-uri'
+require 'JSON'
 require 'sinatra/assetpack'
 
 set :sass, { :load_paths => [ "assets/css/" ] }
@@ -15,6 +17,24 @@ assets do
 
 end
 
+# Routing & stuff
+
 get '/' do
   erb :index
+end
+
+SINGLE_PLAYLIST_URL = "https://gdata.youtube.com/feeds/api/playlists/%s?v=2&alt=json"
+
+get '/playlist/:id' do
+  unless env['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'
+    redirect "/##{params[:id]}"
+  end
+  content_type :json
+  begin
+    open( SINGLE_PLAYLIST_URL % params[:id] ).read
+  rescue OpenURI::HTTPError
+    { status: 'error', errorMessage: "Couldn't find a Playlist with ID '#{params[:id]}'!" }.to_json
+  rescue
+    halt 404 # Just in case
+  end
 end
