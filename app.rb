456fd1b84c5fb70879bb922d1fa668c5869df776
Gabriele Cirulli <gabrycir@gmail.com>
2013-01-06 17:21:11 +0100

    fetch all videos present in a playlist via multiple requests

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'open-uri'
 require 'JSON'
+require 'pry'
 require 'sinatra/assetpack'
 
 set :sass, load_paths: [ "assets/css/" ]
@@ -28,7 +29,7 @@ get '/:id' do
   erb :index
 end
 
-SINGLE_PLAYLIST_FORMAT = "https://gdata.youtube.com/feeds/api/playlists/%s?v=2&alt=json"
+SINGLE_PLAYLIST_FORMAT = "https://gdata.youtube.com/feeds/api/playlists/%s?v=2&max-results=%d&start-index=%d&alt=json"
 
 get '/playlist/:id' do
   playlist_id = URI.encode params[:id]
@@ -38,7 +39,25 @@ get '/playlist/:id' do
 
   content_type :json
   begin
-    open( SINGLE_PLAYLIST_FORMAT % playlist_id ).read
+    per_page = 50 # Upper result count API limit
+    json_result = Hash.new
+    10.times do |offset|
+      api_url = SINGLE_PLAYLIST_FORMAT % [playlist_id, per_page, offset * per_page + 1]
+      json = JSON.parse open( api_url ).read
+
+      if json['feed']['entry']
+        if json_result.empty?
+          json_result.merge! json
+        else
+          json_result['feed']['entry'].concat json['feed']['entry']
+        end
+      else
+        break
+      end
+    end
+    
+    json_result.to_json # Put out the result
+    
   rescue OpenURI::HTTPError
     { status: 'error', errorMessage: "Couldn't find a Playlist with ID '#{params[:id]}'!" }.to_json
   end
