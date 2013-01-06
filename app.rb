87d1baa6307008aa90db5df922b0d662219474c4
Gabriele Cirulli <gabrycir@gmail.com>
2013-01-06 12:47:08 +0100

    make syntax more consistent

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 require 'JSON'
 require 'sinatra/assetpack'
 
-set :sass, { :load_paths => [ "assets/css/" ] }
+set :sass, load_paths: [ "assets/css/" ]
 
 assets do
   serve '/js',  from: 'assets/js'
@@ -23,7 +23,7 @@ get '/' do
   erb :index
 end
 
-SINGLE_PLAYLIST_URL = "https://gdata.youtube.com/feeds/api/playlists/%s?v=2&alt=json"
+SINGLE_PLAYLIST_FORMAT = "https://gdata.youtube.com/feeds/api/playlists/%s?v=2&alt=json"
 
 get '/playlist/:id' do
   playlist_id = URI.encode params[:id]
@@ -33,7 +33,7 @@ get '/playlist/:id' do
 
   content_type :json
   begin
-    open( SINGLE_PLAYLIST_URL % playlist_id ).read
+    open( SINGLE_PLAYLIST_FORMAT % playlist_id ).read
   rescue OpenURI::HTTPError
     { status: 'error', errorMessage: "Couldn't find a Playlist with ID '#{params[:id]}'!" }.to_json
   end
